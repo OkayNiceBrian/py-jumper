e4fce9d6a486ef8a94b1a63f7fa80da888045a79
Brian Jackson <brianjackson1224@gmail.com>
2021-09-30 17:54:42 -0700

    Cleaned up jumper class

--- Jumper.py
+++ Jumper.py
@@ -5,15 +5,15 @@ class Jumper:
     w = 100
     h = 200
     baseXspeed = 2
+    baseJumpForce = 10
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.xspeed = 0
-        self.jumpForce = 10
+        self.jumpForce = Jumper.baseJumpForce
         self.yspeed = 0
         self.isJumping = False
-        self.jumpTimer = 0
 
     def update(self):
         self.x += self.xspeed
